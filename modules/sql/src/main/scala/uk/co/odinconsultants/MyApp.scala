1cd770d271be11c31ce623226e2e0c44c5ff1f70
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-09-17 11:57:53 +0100

    Runs without error.

--- modules/sql/src/main/scala/uk/co/odinconsultants/MyApp.scala
+++ modules/sql/src/main/scala/uk/co/odinconsultants/MyApp.scala
@@ -1,6 +1,9 @@
 package uk.co.odinconsultants
 
 import io.getquill.*
+import io.getquill.context.ExecutionInfo
+
+import java.sql.Connection
 
 object MyApp {
   case class Person(firstName: String, lastName: String, age: Int)
@@ -9,7 +12,9 @@ object MyApp {
   val ctx = new H2JdbcContext(SnakeCase, "ctx")
   import ctx.*
 
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit   = {
+    createPersonTable()
+
     val named                = "Joe"
     inline def somePeople    = quote {
       query[Person].filter(p => p.firstName == lift(named))
@@ -17,5 +22,13 @@ object MyApp {
     val people: List[Person] = run(somePeople)
     // TODO Get SQL
     println(people)
+  }
+  private def createPersonTable(): Unit = {
+    val sql                    =
+      "CREATE TABLE PERSON(FIRST_NAME VARCHAR(255), last_name VARCHAR(255), age int);"
+    val connection: Connection = ctx.dataSource.getConnection
+    connection.createStatement.execute("drop table person;")
+    connection.createStatement.execute(sql)
+    connection.commit()
   }
 }
